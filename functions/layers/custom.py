a9a9a3afd924b5f315c0922b3114932a01e59a11
AKxy4321 <adityakushal23@gmail.com>
2024-08-15 16:13:41 +0530

    c

--- functions/layers/custom.py
+++ functions/layers/custom.py
@@ -12,6 +12,17 @@ def add_custom_fn(model, class_labels):
     x = Model(model.input, x)
     return x
 
+def add_custom_fn_small(model, class_labels):
+    x = Flatten()(model.output)
+    x = Dense(512, activation="relu")(x)
+    x = Dropout(0.4)(x)
+    x = Dense(256, activation="relu")(x)
+    x = Dropout(0.2)(x)
+    x = (Dense(len(class_labels), activation="softmax"))(x)
+
+    x = Model(model.input, x)
+    return x
+
 def add_custom_fn_medium_deep(model, class_labels):                    #Shallow - 1 layer
     x = Flatten()(model.output)                                        #Deep - 3 or more layers
     x = Dense(2048, activation="relu")(x)                              #Small - 512, 256
